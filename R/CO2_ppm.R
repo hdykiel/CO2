92d93a6066eda286f5da584eec541173eebc5109
OpsWorks user hdykiel <hdykiel@rsp1.localdomain>
2020-02-25 17:05:09 +0000

    add user agent

--- R/CO2_ppm.R
+++ R/CO2_ppm.R
@@ -9,9 +9,15 @@
 #' ppm()
 #' > [1] 414.01
 
+# set user agent
+ua <- httr::user_agent("https://github.com/hdykiel/CO2")
 
 ppm <- function() {
-  resp <- httr::GET("http://hqcasanova.com/co2/")
+
+  # send GET request
+  resp <- httr::GET("http://hqcasanova.com/co2/", ua)
+
+  # parse response
   content <- httr::content(resp)
   as.numeric(sub(" ppm", "", content))
 }
